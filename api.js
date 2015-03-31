44b0690d6a723ad40e15f573ff315d6e8f2c89ad
Matt Karl <matt@cloudkid.com>
2015-03-31 10:38:54 -0400

    Auto-generated commit

---
+++ api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "cloudkid.FallbackWorker",
+        "cloudkid.SubWorker",
+        "cloudkid.Worker"
+    ],
+    "modules": [
+        "Worker"
+    ],
+    "allModules": [
+        {
+            "displayName": "Worker",
+            "name": "Worker"
+        }
+    ]
+} };
+});
